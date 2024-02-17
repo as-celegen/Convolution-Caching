694bf0b7d9b601b5a667f12610f9eff2c39ce1a7
as-celegen <ahmedselimcelegen@gmail.com>
2024-02-17 21:27:31 +0300

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include "FullyConnectedLayer.h"
+#include "ConvolutionLayer.h"
+#include "FullyConnectedCachedLayer.h"
+#include "ConvolutionCachedLayer.h"
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
