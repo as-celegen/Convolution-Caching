af0863869ee10295d4a1e3f9849606b49a9637ea
as-celegen <ahmedselimcelegen@gmail.com>
2024-02-17 22:51:30 +0300

    CachedLayer constructor implemented

--- CachedLayer.h
+++ CachedLayer.h
@@ -10,6 +10,22 @@ class CachedLayer {
 	// Array of allowed difference without recalculation with shape [inputSize], if the difference is greater than this value for any given element of input
 	// that value will not be affect the output of this layer
 	double *threshold;
+
+public:
+	CachedLayer(int inputSize, int outputSize, double thresholdValue = 0.0) {
+		nextOutput = new double[outputSize];
+		prevInput = new double[inputSize];
+		threshold = new double[inputSize];
+		for (int i = 0; i < inputSize; i++) {
+			threshold[i] = thresholdValue;
+		}
+	}
+
+	CachedLayer(int inputSize, int outputSize, double* threshold) {
+		nextOutput = new double[outputSize];
+		prevInput = new double[inputSize];
+		this->threshold = threshold;
+	}
 };
 
 
